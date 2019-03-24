67a12abd9dd97464f7ac7585d91ff100e526f106
tusharkadam <tusharop3@gmail.com>
2019-03-24 10:33:37 +0530

    Login Reactive Forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'bwm-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
   }
-
+  initForm(){
+    this.loginForm = this.fb.group({
+      email:'test@gmail.com',
+      password:'random'
+    })
+  }
 }
