ef39fd4adc286c8e00388b6897caa230c8f6c4fd
tusharkadam <tusharop3@gmail.com>
2019-03-26 15:14:43 +0530

    Save booking, update user, update rental ::: Ref  @9.6 notes/ tut vid :::

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -1,6 +1,7 @@
 
 const Booking = require("../models/booking");
 const Rental = require("../models/rental");
+const User = require("../models/user");
 const { normalizeErrors} = require('../helpers/mongoose')
 const moment = require('moment');
 
@@ -22,13 +23,20 @@ exports.createBooking = function(req,res) {
           return res.status(422).send({errors:[{title:'Invalid user!!!', detail : 'Cannot create booking for your Rental!'}]});
           }
           if (isValidBooking(booking, foundRental)) {
+            booking.user = user;
+            booking.rental = foundRental;
             foundRental.bookings.push(booking); // if Booking Dates are not overlapping we will save the bookings back to Database
-            foundRental.save();
-            booking.save();
-            // Update rental, update user
-            return res.json({'created': true});
+
+            booking.save(function(err){
+              if(err){
+                return  res.status(422).send({errors : normalizeErrors(err.errors)});
+              }
+              foundRental.save();
+              User.update({_id: user.id},{$push: {bookings: booking}}, function(){});  // This will select the user with id and push the booking
+              return res.json({startAt: booking.startAt, endAt: booking.endAt});
+            });
           }else{
-            return res.status(422).send({errors:[{title:'Invalid Booking!!!', detail : 'Booking for selected dated is unavailable Try With Different ones'}]});
+            return res.status(422).send({errors:[{title:'Invalid Booking!!!', detail : 'Booking on selected dates are unavailable Try With Different ones'}]});
           }
             })
 
--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -12,18 +12,23 @@ router.get('/secret', UserCtrl.authMiddleware, function (req,res) {
 
 
 router.get('', function(req,res){
-  Rental.find({},function(err, foundRentals){
-    res.json(foundRentals);
-  });
+  Rental.find({})
+        .select('-bookings')
+        .exec(function(err, foundRentals){
+          res.json(foundRentals);
+        });
 });
 
 router.get('/:id', function(req,res){
   const rentalId = req.params.id;
-  Rental.findById(rentalId, function(err, foundRental){
-    if(err){
-      res.status(422).send({errors:[{title:'rental error', detail : 'Could not find rental'}]});
-    }
-    res.json(foundRental);
+  Rental.findById(rentalId)
+              .populate('user', 'username -_id')  // - is used for blocking the id only username will be sent back
+              .populate('bookings','startAt endAt createdAt -_id')
+              .exec(function(err, foundRental){
+        if(err){
+          res.status(422).send({errors:[{title:'rental error', detail : 'Could not find rental'}]});
+          }
+          res.json(foundRental);
   });
 });
 
