5d71b92fdb412bbcb53dab8938985eab0a1e1b48
tusharkadam <tusharop3@gmail.com>
2019-04-15 15:43:56 +0530

    Creating Manage Components ::: Refer 12.6 notes for Details :::

---
+++ src/app/manage/manage.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Routes,RouterModule } from '@angular/router';
+
+import {ManageComponent} from './manage.component';
+import {ManageBookingComponent} from './manage-booking/manage-booking.component';
+import {ManageRentalComponent} from './manage-rental/manage-rental.component';
+
+
+import { AuthGuard } from '../auth/shared/auth.guard';
+
+
+
+const routes: Routes = [
+  {
+    path: 'manage',
+    component : ManageComponent,
+    children : [
+      { path: 'rentals', component: ManageRentalComponent ,canActivate: [AuthGuard]},
+      { path: 'bookings', component: ManageBookingComponent, canActivate: [AuthGuard] }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [
+    ManageComponent,
+    ManageBookingComponent,
+    ManageRentalComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [
+  ],
+})
+export class ManageModule { }
