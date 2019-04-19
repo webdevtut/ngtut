4636268c73142cffd9114c72164ec437ce426aee
tusharkadam <tusharop3@gmail.com>
2019-04-19 20:48:49 +0530

    Rectifies the error in rental Route <Adds missing return > Handling Delete Rental Request and Adding Error Toast for it ::: Ref 12.14 :::

--- src/app/manage/manage-rental/manage-rental.component.ts
+++ src/app/manage/manage-rental/manage-rental.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { RentalService } from '../../rental/shared/rental.service';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { Rental } from '../../rental/shared/rental.model';
+import { HttpErrorResponse} from "@angular/common/http"
 
 @Component({
   selector: 'bwm-manage-rental',
@@ -12,7 +14,11 @@ export class ManageRentalComponent implements OnInit {
   rentals: Rental[];
   rentalDeleteIndex: number;
 
-  constructor(private rentalService: RentalService) { }
+  constructor(private rentalService: RentalService,
+              private toastr: ToastsManager,
+              private vcr: ViewContainerRef ) {
+                this.toastr.setRootViewContainerRef(vcr);
+               }
 
   ngOnInit() {
     this.rentalService.getUserRentals().subscribe(
@@ -21,8 +27,16 @@ export class ManageRentalComponent implements OnInit {
       },
       ()=>{
 
-      }
-    )
-  }
-
+      })
+    }
+deleteRental(rentalId: string){
+  this.rentalService.deleteRental(rentalId).subscribe(
+    ()=>{
+      this.rentals.splice(this.rentalDeleteIndex, 1);
+    },
+    (errorResponse:HttpErrorResponse) =>{
+      this.toastr.error(errorResponse.error.errors[0].detail, 'Failed');
+    }
+  )
+}
 }
--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -29,4 +29,8 @@ public createRental(rental: Rental): Observable<any>{
 public getUserRentals(): Observable<any>{
   return this.http.get('/api/v1/rentals/manage');
 }
+
+public deleteRental(rentalId: string): Observable<any>{
+  return this.http.delete(`/api/v1/rentals/${rentalId}`)
+}
 }
