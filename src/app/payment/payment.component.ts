8b7ff9abd6f2f5e4226695ce2d7126f37f5df78d
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2019-09-19 15:12:12 +0530

    31.4 Stripe Form part 2

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ElementRef } from '@angular/core';
 import { environment } from '../../environments/environment'
 
 @Component({
@@ -10,12 +10,28 @@ export class PaymentComponent implements OnInit {
   stripe: any;
   elements: any;
 
+  @ViewChild('cardNumber') cardNumRef : ElementRef;
+  @ViewChild('cardExp') cardExpRef : ElementRef;
+  @ViewChild('cardCVV') cardCVVRef : ElementRef;
+
+cardNumber: any;
+cardExp: any;
+cardCVV : any;
+
   constructor() {
   this.stripe = Stripe(environment.STRIPE_PK);
   this.elements = this.stripe.elements();
  }
 
   ngOnInit() {
+    this.cardNumber = this.elements.create('cardNumber', {style});
+    this.cardNumber.mount(this.cardNumRef.nativeElement);
+
+    this.cardExp = this.elements.create('cardExpiry', {style});
+    this.cardExp.mount(this.cardExpRef.nativeElement);
+
+    this.cardCVV = this.elements.create('cardCvc', {style});
+    this.cardCVV.mount(this.cardCVVRef.nativeElement);
   }
 
   onSubmit(){
@@ -23,3 +39,18 @@ export class PaymentComponent implements OnInit {
   }
 
 }
+
+const style = {
+  base: {
+    iconColor: '#666EE8',
+    color: '#31325F',
+    lineHeight: '40px',
+    fontWeight: 300,
+    fontFamily: 'Helvetica Neue',
+    fontSize: '15px',
+
+    '::placeholder': {
+      color: '#CFD7E0',
+    },
+  },
+};
