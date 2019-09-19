223f5b553c4a8cc134524d1be31b249bc510c6f6
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2019-09-19 23:03:45 +0530

    31.5 5. Handling Error in Stripe

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -18,9 +18,13 @@ cardNumber: any;
 cardExp: any;
 cardCVV : any;
 
+error:  '';
+
   constructor() {
   this.stripe = Stripe(environment.STRIPE_PK);
   this.elements = this.stripe.elements();
+
+  this.onChange = this.onChange.bind(this);
  }
 
   ngOnInit() {
@@ -32,6 +36,29 @@ cardCVV : any;
 
     this.cardCVV = this.elements.create('cardCvc', {style});
     this.cardCVV.mount(this.cardCVVRef.nativeElement);
+
+    this.cardNumber.addEventListener('change', this.onChange);
+    this.cardExp.addEventListener('change', this.onChange);
+    this.cardCVV.addEventListener('change', this.onChange);
+  }
+
+  ngOnDestroy(){
+    this.cardNumber.removeEventListener('change', this.onChange);
+    this.cardExp.removeEventListener('change', this.onChange);
+    this.cardCVV.removeEventListener('change', this.onChange);
+
+    this.cardNumber.destroy();
+    this.cardExp.destroy();
+    this.cardCVV.destroy();
+  }
+
+  onChange({error}){
+    debugger;
+    if(error) {
+      this.error = error.message;
+    }else{
+      this.error = '';
+    }
   }
 
   onSubmit(){
