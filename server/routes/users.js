b71bec49fed00fdd7a4593d1c7968e4f24a914d9
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2019-10-08 16:01:25 +0530

    31.21 [Node] Profile Page Endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,9 @@ const User = require('../controllers/user')
 const router = express.Router();
 
 
+router.get('/:id', User.authMiddleware, User.getUser);
+
+
 router.post('/auth', User.auth);
 router.post('/register',User.register);
 
